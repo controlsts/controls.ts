8d6e5da3230fb995756f382ee7486bbbe604b638
swcho <sungwoo.cho.dev@gmail.com>
2015-09-14 22:53:35 +0900

    less js combine layout

--- declarative/test_layout.js
+++ declarative/test_layout.js
@@ -1,72 +1,81 @@
 /// <reference path="../controls.ts"/>
 var App;
 (function (App) {
-    var root = document.body;
-    root.id = "idRoot";
-    var lcgRoot = Controls.LayoutGroupControl({
-        el: root,
-        controls: [
-            Controls.LayoutControl({
-                itemDrawers: [
-                    function (aElement, aIndex) {
-                        aElement.id = "idStatus";
-                        aElement.innerText = "-status";
-                        return 2 /* KFocusAble */;
-                    },
-                ]
-            }),
-            Controls.LayoutGroupControl({
-                id: 'idContent',
-                stretchWidth: true,
-                stretchHeight: true,
-                orientation: 2 /* EHorizontal */,
-                controls: [
-                    Controls.LayoutControl({
-                        width: 100,
-                        stretchHeight: true,
-                        itemDrawers: [
-                            function (aElement, aIndex) {
-                                aElement.id = "-menu-upper-item1";
-                                aElement.innerText = "-menu-upper-item1";
-                                return 2 /* KFocusAble */;
-                            },
-                            function (aElement, aIndex) {
-                                aElement.id = "-menu-lower-item1";
-                                aElement.innerText = "-menu-lower-item1";
-                                return 2 /* KFocusAble */;
-                            }
-                        ]
-                    }),
-                    Controls.LayoutControl({
-                        stretchWidth: true,
-                        stretchHeight: true,
-                        itemDrawers: [
-                            function (aElement, aIndex) {
-                                aElement.id = "-content1-item1";
-                                aElement.innerHTML = "-content1-item1";
-                                return 2 /* KFocusAble */;
-                            },
-                            function (aElement, aIndex) {
-                                aElement.id = "-content2-item1";
-                                aElement.innerHTML = "-content2-item1";
-                                return 2 /* KFocusAble */;
-                            }
-                        ]
-                    })
-                ]
-            }),
-            Controls.LayoutControl({
-                stretchWidth: true,
-                itemDrawers: [
-                    function (aElement, aIndex) {
-                        aElement.id = "idFooter";
-                        aElement.innerText = "Footer";
-                        return 2 /* KFocusAble */;
-                    },
-                ]
-            })
-        ]
+    console.log(window.innerWidth, window.innerHeight);
+    var lcgRoot;
+    window.addEventListener('resize', function () {
+        less.modifyVars({
+            '@SCREEN_WIDTH': window.innerWidth + 'px',
+            '@SCREEN_HEIGHT': window.innerHeight + 'px'
+        });
+        lcgRoot.draw();
+    }, true);
+    less.modifyVars({
+        '@SCREEN_WIDTH': window.innerWidth + 'px',
+        '@SCREEN_HEIGHT': window.innerHeight + 'px'
+    }).then(function () {
+        console.log('start');
+        var root = document.body;
+        root.id = "idRoot";
+        lcgRoot = Controls.LayoutGroupControl({
+            el: root,
+            controls: [
+                Controls.LayoutControl({
+                    itemDrawers: [
+                        function (aElement, aIndex) {
+                            aElement.id = "idStatus";
+                            aElement.innerText = "-status";
+                            return 2 /* KFocusAble */;
+                        },
+                    ]
+                }),
+                Controls.LayoutGroupControl({
+                    id: 'idContent',
+                    orientation: 2 /* EHorizontal */,
+                    controls: [
+                        Controls.LayoutControl({
+                            width: 100,
+                            itemDrawers: [
+                                function (aElement, aIndex) {
+                                    aElement.id = "-menu-upper-item1";
+                                    aElement.innerText = "-menu-upper-item1";
+                                    return 2 /* KFocusAble */;
+                                },
+                                function (aElement, aIndex) {
+                                    aElement.id = "-menu-lower-item1";
+                                    aElement.innerText = "-menu-lower-item1";
+                                    return 2 /* KFocusAble */;
+                                }
+                            ]
+                        }),
+                        Controls.LayoutControl({
+                            itemDrawers: [
+                                function (aElement, aIndex) {
+                                    aElement.id = "-content1-item1";
+                                    aElement.innerHTML = "-content1-item1";
+                                    return 2 /* KFocusAble */;
+                                },
+                                function (aElement, aIndex) {
+                                    aElement.id = "-content2-item1";
+                                    aElement.innerHTML = "-content2-item1";
+                                    return 2 /* KFocusAble */;
+                                }
+                            ]
+                        })
+                    ]
+                }),
+                Controls.LayoutControl({
+                    itemDrawers: [
+                        function (aElement, aIndex) {
+                            aElement.id = "idFooter";
+                            aElement.innerText = "Footer";
+                            return 2 /* KFocusAble */;
+                        },
+                    ]
+                })
+            ]
+        });
+        Controls.runRoot(lcgRoot);
     });
-    Controls.runRoot(lcgRoot);
 })(App || (App = {}));
 //# sourceMappingURL=test_layout.js.map
--- declarative/test_layout.ts
+++ declarative/test_layout.ts
@@ -1,74 +1,83 @@
 /// <reference path="../controls.ts"/>
 
 module App {
+    console.log(window.innerWidth, window.innerHeight);
+    var lcgRoot;
+    window.addEventListener('resize', function() {
+        less.modifyVars({
+            '@SCREEN_WIDTH': window.innerWidth + 'px',
+            '@SCREEN_HEIGHT': window.innerHeight + 'px'
+        });
+        lcgRoot.draw();
+    }, true);
+    less.modifyVars({
+        '@SCREEN_WIDTH': window.innerWidth + 'px',
+        '@SCREEN_HEIGHT': window.innerHeight + 'px'
+    }).then(function() {
+        console.log('start');
+        var root = document.body;
+        root.id = "idRoot";
 
-    var root = document.body;
-    root.id = "idRoot";
+        lcgRoot = Controls.LayoutGroupControl({
+            el: root,
+            controls: [
+                Controls.LayoutControl({
+                    itemDrawers: [
+                        function (aElement: HTMLElement, aIndex: number) {
+                            aElement.id = "idStatus";
+                            aElement.innerText = "-status";
+                            return Controls.TFocusInfo.KFocusAble;
+                        },
+                    ]
+                }),
+                Controls.LayoutGroupControl({
+                    id: 'idContent',
+                    orientation: Controls.TParamOrientation.EHorizontal,
+                    controls: [
+                        Controls.LayoutControl({
+                            width: 100,
+                            itemDrawers: [
+                                function (aElement: HTMLElement, aIndex: number) {
+                                    aElement.id = "-menu-upper-item1";
+                                    aElement.innerText = "-menu-upper-item1";
+                                    return Controls.TFocusInfo.KFocusAble;
+                                },
+                                function (aElement: HTMLElement, aIndex: number) {
+                                    aElement.id = "-menu-lower-item1";
+                                    aElement.innerText = "-menu-lower-item1";
+                                    return Controls.TFocusInfo.KFocusAble;
+                                }
+                            ]
+                        }),
+                        Controls.LayoutControl({
+                            itemDrawers: [
+                                function (aElement: HTMLElement, aIndex: number) {
+                                    aElement.id = "-content1-item1";
+                                    aElement.innerHTML = "-content1-item1";
+                                    return Controls.TFocusInfo.KFocusAble;
+                                },
+                                function (aElement: HTMLElement, aIndex: number) {
+                                    aElement.id = "-content2-item1";
+                                    aElement.innerHTML = "-content2-item1";
+                                    return Controls.TFocusInfo.KFocusAble;
+                                }
+                            ]
+                        })
+                    ]
+                }),
+                Controls.LayoutControl({
+                    itemDrawers: [
+                        function (aElement: HTMLElement, aIndex: number) {
+                            aElement.id = "idFooter";
+                            aElement.innerText = "Footer";
+                            return Controls.TFocusInfo.KFocusAble;
+                        },
+                    ]
+                })
+            ]
+        });
 
-    var lcgRoot = Controls.LayoutGroupControl({
-        el: root,
-        controls: [
-            Controls.LayoutControl({
-                itemDrawers: [
-                    function (aElement: HTMLElement, aIndex: number) {
-                        aElement.id = "idStatus";
-                        aElement.innerText = "-status";
-                        return Controls.TFocusInfo.KFocusAble;
-                    },
-                ]
-            }),
-            Controls.LayoutGroupControl({
-                id: 'idContent',
-                stretchWidth: true,
-                stretchHeight: true,
-                orientation: Controls.TParamOrientation.EHorizontal,
-                controls: [
-                    Controls.LayoutControl({
-                        width: 100,
-                        stretchHeight: true,
-                        itemDrawers: [
-                            function (aElement: HTMLElement, aIndex: number) {
-                                aElement.id = "-menu-upper-item1";
-                                aElement.innerText = "-menu-upper-item1";
-                                return Controls.TFocusInfo.KFocusAble;
-                            },
-                            function (aElement: HTMLElement, aIndex: number) {
-                                aElement.id = "-menu-lower-item1";
-                                aElement.innerText = "-menu-lower-item1";
-                                return Controls.TFocusInfo.KFocusAble;
-                            }
-                        ]
-                    }),
-                    Controls.LayoutControl({
-                        stretchWidth: true,
-                        stretchHeight: true,
-                        itemDrawers: [
-                            function (aElement: HTMLElement, aIndex: number) {
-                                aElement.id = "-content1-item1";
-                                aElement.innerHTML = "-content1-item1";
-                                return Controls.TFocusInfo.KFocusAble;
-                            },
-                            function (aElement: HTMLElement, aIndex: number) {
-                                aElement.id = "-content2-item1";
-                                aElement.innerHTML = "-content2-item1";
-                                return Controls.TFocusInfo.KFocusAble;
-                            }
-                        ]
-                    })
-                ]
-            }),
-            Controls.LayoutControl({
-                stretchWidth: true,
-                itemDrawers: [
-                    function (aElement: HTMLElement, aIndex: number) {
-                        aElement.id = "idFooter";
-                        aElement.innerText = "Footer";
-                        return Controls.TFocusInfo.KFocusAble;
-                    },
-                ]
-            })
-        ]
+        Controls.runRoot(lcgRoot);
+
     });
-
-    Controls.runRoot(lcgRoot);
 }
